778292d52b3e94fe8248b28fa8ebd95a7cc53764
Austin C <austins.accts@gmail.com>
2018-09-27 19:10:00 -0700

    path and add files, config webpack and json

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,110 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry:'./src/index.js'
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: './src/index.html',
+      filename: 'index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    }),
+
+    new HtmlWebpackPlugin({
+    inject: 'body',
+    template: './src/tour.html',
+    filename: 'tour.html',
+    minify: {
+      removeComments: true,
+      collapseWhitespace: true
+    }
+  }),
+
+  new HtmlWebpackPlugin({
+    inject: 'body',
+    template: './src/contact.html',
+    filename: 'contact.html',
+    minify: {
+      removeComments: true,
+      collapseWhitespace: true
+    }
+  }),
+
+  new HtmlWebpackPlugin({
+    inject: 'body',
+    template: './src/about.html',
+    filename: 'about.html',
+    minify: {
+      removeComments: true,
+      collapseWhitespace: true
+    }
+  }),
+
+    new UglifyJsPlugin(),
+    new CleanWebpackPlugin(['dist'])
+  ]
+
 };
